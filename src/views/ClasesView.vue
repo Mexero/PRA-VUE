3c5783695c93ff90dea35a4746efc35b04f40484
Mexero <mrmexeriko@gmail.com>
2025-05-10 21:57:16 +0200

    Lógica de clases terminada

--- src/views/ClasesView.vue
+++ src/views/ClasesView.vue
@@ -6,35 +6,55 @@ const router = useRouter()
 
 const claseCargada = ref('');
 const tabla = ref({ titulos: [], filas: [] });
-const descripcion = ref('');
-const tiradas = ref({ nombre: '', descripcion: '' });
+const descripcion = ref([]);
+const tiradas = ref({ nombre: '', descripcion: [] });
 const rasgos = ref([]);
 const subclasesIndex = ref([]);
 const subclasesCargadas = ref([]);
 
-function cargarClase(clase) {
-    fetch(`data/json/${clase}/${clase}.json`)
-        .then(res => res.json())
-        .then(data => {
-            //Borro subclases anteriores //ESTO A MEDIAS
-            subclasesCargadas.value = [];
+let ResetBotonesSubclasesAlCambiarClase = false; //Poner esta key a todo lo que quieras que se resetee del todo al cargar otra clase. SOLO IMPRESCINDIBLE
 
-            //cargo clase
-            claseCargada.value = clase;
-            tabla.value = data.tablaClase;
-            descripcion.value = data.descripcion;
-            tiradas.value = data.tiradasSalvacion;
-            rasgos.value = data.rasgos;
-            subclasesIndex.value = data.subclases;
-        })
-        .catch(err => console.error("Error al cargar", err));
+
+//=============FETCH Y MANIPULACIÓN DE DATOS===========
+async function cargarClase(clase) {
+    if (clase === claseCargada.value) {
+        console.log("No se ha cargado la clase porque ya estaba cargada...");
+        return 1;
+    }
+    console.time('fetchData');
+    try {
+        const res = await fetch(`data/json/${clase}/${clase}.json`);
+        if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
+
+        const data = await res.json();
+
+        // Borro subclases anteriores
+        subclasesCargadas.value = [];
+
+        // Cargo clase
+        claseCargada.value = clase;
+        tabla.value = data.tablaClase;
+        descripcion.value = data.descripcion;
+        tiradas.value = data.tiradasSalvacion;
+        rasgos.value = data.rasgos;
+        subclasesIndex.value = data.subclases;
+    } catch (err) {
+        console.error("Error al cargar clase:", err);
+    }
+    ResetBotonesSubclasesAlCambiarClase = !ResetBotonesSubclasesAlCambiarClase; //Al cargar
+
+    console.timeEnd('fetchData');
 }
 
-async function toggleSubclase(ruta) {
+async function toggleSubclase(ruta, e) {
     const existente = subclasesCargadas.value.find(s => s.ruta === ruta);
 
     if (existente) {
         existente.activo = !existente.activo;
+        if (existente.activo) { e.target.className = "btn-active" }
+        else {
+            e.target.className = "btn-inactive"
+        }
     } else {
         const datosSubclase = await fetch(`/data/json/${claseCargada.value}/subclases/${ruta}.json`)
             .then(res => res.json())
@@ -49,15 +69,31 @@ async function toggleSubclase(ruta) {
                 activo: true,
                 subclase: datosSubclase
             });
+            console.log(`Cargado ${ruta}`)
+            e.target.className = "btn-active"
         }
     }
+
+}
+
+
+//esto no funciona aun
+function isSubclassLoaded(ruta) {
+    const subclase = subclasesCargadas.value.find(s => s.ruta === ruta);
+    if (subclase && subclase.activo)
+        return true
+    else {
+        return false
+    }
+
 }
 
 
 
-
+//==============EN CARGA===========
 onMounted(() => {
     cargarClase('entrenador');
+
 });
 
 </script>
@@ -68,13 +104,17 @@ onMounted(() => {
         <div class="top-section">
             <div class="buttons">
                 <div class="class-buttons">
-                    <button @click="cargarClase('entrenador')">Entrenador</button><br>
-                    <button @click="console.log(' pinga')">Inventor NO VA</button>
-                    <button @click="console.log(' pinga')">Mentalista NO VA</button>
-                    <button @click="cargarClase('luchador')">Luchador</button>
+                    <button @click="cargarClase('entrenador')"
+                        :class="claseCargada === 'entrenador' ? 'btn-active' : 'btn-inactive'">Entrenador</button><br>
+                    <button @click="console.log(' pinga')"
+                        :class="claseCargada === 'inventor' ? 'btn-active' : 'btn-inactive'">Inventor NO VA</button>
+                    <button @click="console.log(' pinga')"
+                        :class="claseCargada === 'mentalista' ? 'btn-active' : 'btn-inactive'">Mentalista NO VA</button>
+                    <button @click="cargarClase('luchador')"
+                        :class="claseCargada === 'luchador' ? 'btn-active' : 'btn-inactive'">Luchador</button>
                 </div>
-                <div class="subclass-buttons">
-                    <button v-for="(subclase, i) in subclasesIndex" @click="toggleSubclase(subclase.ruta)">{{
+                <div class="subclass-buttons" :key="ResetBotonesSubclasesAlCambiarClase">
+                    <button v-for="(subclase) in subclasesIndex" @click="e => (toggleSubclase(subclase.ruta, e))">{{
                         subclase.acortado }}</button>
                 </div>
             </div>
@@ -112,15 +152,19 @@ onMounted(() => {
 
             </div>
         </div>
+
+
+
         <div class="bottom-section">
             <!-- Rasgos -->
             <div>
-                <p v-html="descripcion"></p>
+                <p v-for="(parrafo) in Array.isArray(descripcion) ? descripcion : [descripcion]" v-html="parrafo"></p>
 
                 <details open>
                     <summary class="featureHead">{{ tiradas.nombre }}</summary>
                     <div class="detailsBlock">
-                        <p class="classData">{{ tiradas.descripcion }}</p>
+                        <p v-for="(parrafo) in Array.isArray(tiradas.descripcion) ? tiradas.descripcion : [tiradas.descripcion]"
+                            v-html="parrafo" class="classData"></p>
                     </div>
                 </details>
 
@@ -135,12 +179,35 @@ onMounted(() => {
                                 :key="j" v-html="parrafo">
                             </p>
                         </div>
+                        <!-- subclase -->
                         <div v-if="rasgo.esSubclase" :id="'subclase' + rasgo.nivel">
-                            <div v-for="(subclase, index) in subclasesCargadas">
-                                <p>{{ subclase.subclase.nombre }}</p>
-                                <div v-for="(rasgoSub, index) in subclase.subclase.rasgos">
-                                    <p v-if="rasgoSub.nivel === rasgo.nivel">{{ rasgoSub.nombre }}</p>
-                                </div>
+                            <div v-for="(subclase) in subclasesCargadas">
+                                <template v-if="subclase.activo">
+                                    <template v-for="(rasgoSub) in subclase.subclase.rasgos">
+                                        <div v-if="rasgoSub.nivel === rasgo.nivel">
+                                            <details open>
+                                                <summary>{{ rasgoSub.nombre }}</summary>
+                                                <p>{{ rasgoSub.nombreSubclase }} {{ rasgo.nivel }}</p>
+                                                <!-- tiene más de 1 rasgo al nivel -->
+                                                <template v-if="rasgoSub.tieneSubrasgos">
+                                                    <div v-for="(subrasgo) in rasgoSub.contenido">
+                                                        <details open>
+                                                            <summary>{{ subrasgo.nombre }}</summary>
+                                                            <p v-for="(parrafo) in Array.isArray(subrasgo.descripcion) ? subrasgo.descripcion : [subrasgo.descripcion]"
+                                                                v-html="parrafo">
+                                                            </p>
+                                                        </details>
+                                                    </div>
+                                                </template>
+                                                <!-- tiene sólo 1 rasgo al nivel -->
+                                                <template v-else>
+                                                    <p v-for="(parrafo) in Array.isArray(rasgoSub.contenido) ? rasgoSub.contenido : [rasgoSub.contenido]"
+                                                        v-html="parrafo"> </p>
+                                                </template>
+                                            </details>
+                                        </div>
+                                    </template>
+                                </template>
                             </div>
                         </div>
                     </div>
@@ -195,7 +262,7 @@ onMounted(() => {
 }
 
 
-.subclass-buttons button.active {
+.btn-active {
     background-color: tomato;
 }
 
@@ -235,7 +302,7 @@ tr:nth-of-type(2n) {
 
 /*=========PARTE DE ABAJO==========*/
 .bottom-section {
-    background-image: url("https://i.pinimg.com/474x/f4/1a/95/f41a95cba3b00c5b9d563400ddc58a26.jpg");
+    background-color: lightblue;
     display: flex;
     flex-direction: column;
     gap: 10px;
