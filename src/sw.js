0e5a127818153ec5d225c0b2a030f36128447d06
Mexero <mrmexeriko@gmail.com>
2025-08-11 18:15:15 +0200

    versión 0

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@ import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 // Version caches
 const JSON_CACHE_VERSION = 'v1';
-const SQLITE_CACHE_VERSION = 'v2';
+const SQLITE_CACHE_VERSION = 'v1';
 const VIEW_CACHE_VERSION = 'v1';
 const RESOURCES_CACHE_VERSION = 'v1';
 
